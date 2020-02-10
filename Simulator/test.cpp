e86ad84d802ad134bd075545d3b949eb7ea7f246
pshmx2007 <psh19940403@gmail.com>
2020-02-11 03:27:39 +0900

    RC-4 update main.cpp
    with some test code

---
+++ Simulator/test.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <memory>
+#include <vector>
+
+// Important Note:
+// A lot of classes have "Task" in their name.
+// This Task is simply a name used in "Task Based Parallelism" programming model.
+// It does not have to do with "Simulation tasks".
+// Everything is a task.
+
+int main()
+{
+    printf("hello from CPSim!\n");
+    return 0;
+}
