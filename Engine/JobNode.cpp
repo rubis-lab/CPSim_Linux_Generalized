ed6bbb4ab8811d2a9c306f404435fb74392cfe21
Sehyo <Alex_Noble@hotmail.com>
2020-05-20 03:01:23 +0900

    Added graph data structure for offline guider.

---
+++ Engine/JobNode.cpp
@@ -0,0 +1,37 @@
+#include "JobNode.h"
+
+JobNode::JobNode(std::shared_ptr<Job> job)
+{
+	this->_job = job;
+}
+
+void JobNode::insertEdge(std::shared_ptr<JobNode> dest, bool isDeterministic)
+{
+	EdgeInfo info;
+	info.isDeterministic = isDeterministic;
+	this->_edges.push_back(std::make_pair(dest, info));
+}
+
+std::vector<std::shared_ptr<JobNode>> JobNode::getDeterministicEdges()
+{
+	std::vector<std::shared_ptr<JobNode>> deterministicEdges;
+	for (auto edge : this->_edges)
+		if (edge.second.isDeterministic)
+			deterministicEdges.push_back(edge.first);
+	return deterministicEdges;
+}
+
+
+std::vector<std::shared_ptr<JobNode>> JobNode::getNonDeterministicEdges()
+{
+	std::vector<std::shared_ptr<JobNode>> nonDeterministicEdges;
+	for (auto edge : this->_edges)
+		if (!edge.second.isDeterministic)
+			nonDeterministicEdges.push_back(edge.first);
+	return nonDeterministicEdges;
+}
+
+std::vector<std::pair<std::shared_ptr<JobNode>, EdgeInfo>> JobNode::getAllEdges()
+{
+	return this->_edges;
+}
---
+++ Engine/JobNode.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include "Job.h"
+
+typedef struct EdgeInfo
+{
+	bool isDeterministic = true;
+	// ... Other stuff?
+} EdgeInfo;
+
+class JobNode
+{
+	std::vector<std::pair<std::shared_ptr<JobNode>, EdgeInfo>> _edges;
+	std::shared_ptr<Job> _job = nullptr; // Actual job we represent.
+public:
+	void insertEdge(std::shared_ptr<JobNode> dest, bool isDeterministic);
+	JobNode(std::shared_ptr<Job> job);
+	std::vector<std::shared_ptr<JobNode>> getDeterministicEdges();
+	std::vector<std::shared_ptr<JobNode>> getNonDeterministicEdges();
+	std::vector<std::pair<std::shared_ptr<JobNode>, EdgeInfo>> getAllEdges();
+};
