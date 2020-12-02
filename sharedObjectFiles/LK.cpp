89a780fe68c1dbf358d8ad36b743a716229bd1b4
Kimdo-765 <ralehgus123@gmail.com>
2020-12-02 20:57:39 +0900

    54:32

--- sharedObjectFiles/LK.cpp
+++ sharedObjectFiles/LK.cpp
@@ -55,7 +55,7 @@ void sim_main()
     }
     else if ((rtU->read1 < 0.0) && (rtU->read1 < -10000.0))
     {
-      rtDW->w3 = 10000.0;
+      rtDW->w3 = 2000.0;
     }
     else
     {
@@ -67,7 +67,7 @@ void sim_main()
 
     if ((rtU->read1 > 0.0) && (rtU->read1 > 10000.0))
     {
-      rtDW->w4 = 10000.0;
+      rtDW->w4 = 2000.0;
     }
     else if ((rtU->read1 > 0.0) && (rtU->read1 <= 10000.0))
     {
