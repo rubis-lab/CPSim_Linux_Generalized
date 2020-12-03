e7d0fb6549928bf4f97abd5ea814d40de9ece308
jukrang0408 <kma19009@kma.ac.kr>
2020-12-04 02:46:39 +0900

    lk file

--- sharedObjectFiles/LK/LK.cpp
+++ sharedObjectFiles/LK/LK.cpp
@@ -13,23 +13,23 @@ void sim_main()
     {
       rtDW->w3 = 0.0;
     }
-    else if ((rtU->read1 < 0.0) && (rtU->read1 < -10000.0))
+    else if ((rtU->read1 < 0.0) && (rtU->read1 < -2000.0))
     {
-      rtDW->w3 = 10000.0;
+      rtDW->w3 = 2000.0;
     }
     else
     {
-      if ((rtU->read1 < 0.0) && (rtU->read1 >= -10000.0))
+      if ((rtU->read1 < 0.0) && (rtU->read1 >= -2000.0))
       {
         rtDW->w3 = rtU->read1 * -1;
       }
     }
 
-    if ((rtU->read1 > 0.0) && (rtU->read1 > 10000.0))
+    if ((rtU->read1 > 0.0) && (rtU->read1 > 2000.0))
     {
-      rtDW->w4 = 10000.0;
+      rtDW->w4 = 2000.0;
     }
-    else if ((rtU->read1 > 0.0) && (rtU->read1 <= 10000.0))
+    else if ((rtU->read1 > 0.0) && (rtU->read1 <= 2000.0))
     {
       rtDW->w4 = rtU->read1;
     }
