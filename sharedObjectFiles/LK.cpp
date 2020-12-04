036865e5ccf8581c7683460ae1a4eefb8ccd1731
akyeast <akyeast@gmail.com>
2020-12-04 22:31:42 +0900

    akyeast prj1

--- sharedObjectFiles/LK.cpp
+++ sharedObjectFiles/LK.cpp
@@ -42,34 +42,34 @@ extern "C" void sim_main();
 
 void sim_main()
 {
-  if (rtU->read2 <= 5000.0)
+  if (rtU->read2 <= 2500.0) //
   {
     rtDW->w3 = 0.0;
     rtDW->w4 = 0.0;
   }
   else
   {
-    if (rtU->read1 > 0.0)
+    if (rtU->read1 > 0.0) //
     {
       rtDW->w3 = 0.0;
     }
-    else if ((rtU->read1 < 0.0) && (rtU->read1 < -10000.0))
+    else if ((rtU->read1 < 0.0) && (rtU->read1 < -5000.0))
     {
-      rtDW->w3 = 10000.0;
+      rtDW->w3 = 5000.0;
     }
     else
     {
-      if ((rtU->read1 < 0.0) && (rtU->read1 >= -10000.0))
+      if ((rtU->read1 < 0.0) && (rtU->read1 >= -5000.0))
       {
         rtDW->w3 = rtU->read1 * -1;
       }
     }
 
-    if ((rtU->read1 > 0.0) && (rtU->read1 > 10000.0))
+    if ((rtU->read1 > 0.0) && (rtU->read1 > 5000.0))
     {
-      rtDW->w4 = 10000.0;
+      rtDW->w4 = 5000.0;
     }
-    else if ((rtU->read1 > 0.0) && (rtU->read1 <= 10000.0))
+    else if ((rtU->read1 > 0.0) && (rtU->read1 <= 5000.0))
     {
       rtDW->w4 = rtU->read1;
     }
--- sharedObjectFiles/LK/LK.cpp
+++ sharedObjectFiles/LK/LK.cpp
@@ -2,34 +2,34 @@
 
 void sim_main()
 {
-  if (rtU->read2 <= 5000.0)
+  if (rtU->read2 <= 2500.0) //
   {
     rtDW->w3 = 0.0;
     rtDW->w4 = 0.0;
   }
   else
   {
-    if (rtU->read1 > 0.0)
+    if (rtU->read1 > 0.0) //
     {
       rtDW->w3 = 0.0;
     }
-    else if ((rtU->read1 < 0.0) && (rtU->read1 < -10000.0))
+    else if ((rtU->read1 < 0.0) && (rtU->read1 < -5000.0))
     {
-      rtDW->w3 = 10000.0;
+      rtDW->w3 = 5000.0;
     }
     else
     {
-      if ((rtU->read1 < 0.0) && (rtU->read1 >= -10000.0))
+      if ((rtU->read1 < 0.0) && (rtU->read1 >= -5000.0))
       {
         rtDW->w3 = rtU->read1 * -1;
       }
     }
 
-    if ((rtU->read1 > 0.0) && (rtU->read1 > 10000.0))
+    if ((rtU->read1 > 0.0) && (rtU->read1 > 5000.0))
     {
-      rtDW->w4 = 10000.0;
+      rtDW->w4 = 5000.0;
     }
-    else if ((rtU->read1 > 0.0) && (rtU->read1 <= 10000.0))
+    else if ((rtU->read1 > 0.0) && (rtU->read1 <= 5000.0))
     {
       rtDW->w4 = rtU->read1;
     }
