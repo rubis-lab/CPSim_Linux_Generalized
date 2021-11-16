54524cee31ef34867c03655e853430703ae59b18
tjswns494 <tjswns494@naver.com>
2021-11-16 15:09:07 +0900

    (CU-1t2x0d4[complete]) make process

--- CPSim_GUI/src/org/eclipse/cpsim/Diagram/util/ShellCommander.java
+++ CPSim_GUI/src/org/eclipse/cpsim/Diagram/util/ShellCommander.java
@@ -6,6 +6,6 @@ import java.io.InputStreamReader;
 public class ShellCommander {
 	public static void shellCmdArray(String[] strings) throws Exception {
 		Runtime runtime = Runtime.getRuntime();
-	    Process process = runtime.exec(command);
+		Process process = runtime.exec(strings);
 	}
 }
