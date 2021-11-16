90ff3a8ee707f63249fa3eb95fa8066605a41c4c
tjswns494 <tjswns494@naver.com>
2021-11-16 15:09:39 +0900

    (CU-1t2x0db[complete]) wait process

--- CPSim_GUI/src/org/eclipse/cpsim/Diagram/util/ShellCommander.java
+++ CPSim_GUI/src/org/eclipse/cpsim/Diagram/util/ShellCommander.java
@@ -7,5 +7,6 @@ public class ShellCommander {
 	public static void shellCmdArray(String[] strings) throws Exception {
 		Runtime runtime = Runtime.getRuntime();
 		Process process = runtime.exec(strings);
+		process.waitFor();
 	}
 }
