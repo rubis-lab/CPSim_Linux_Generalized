d347d7338dd77d703fda464ca4880a47ebcc7362
tjswns494 <tjswns494@naver.com>
2021-11-16 14:53:41 +0900

    (CU-1t2wx17[complete]) set layout

--- CPSim_GUI/src/org/eclipse/cpsim/Diagram/dialog/SWCPropertiesDialog.java
+++ CPSim_GUI/src/org/eclipse/cpsim/Diagram/dialog/SWCPropertiesDialog.java
@@ -214,7 +214,7 @@ public class SWCPropertiesDialog extends PropertiesDialog {
 		Composite areaCallbackType = (Composite) super.createDialogArea(parent);
 		Group groupCallbackType = new Group(areaCallbackType, SWT.SHADOW_IN);
 		groupCallbackType.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, false, false, 1, 1));
-
+		groupCallbackType.setLayout(new RowLayout(SWT.HORIZONTAL));
 
 
 		// Period
