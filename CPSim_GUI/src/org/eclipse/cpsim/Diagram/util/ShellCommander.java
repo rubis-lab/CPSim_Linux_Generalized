4a59b30831f0be14ee8c1328a21df2d18a43a74c
tjswns494 <tjswns494@naver.com>
2021-11-16 15:07:09 +0900

    (CU-1t2x0a0[complete]) import buffered reader

--- CPSim_GUI/src/org/eclipse/cpsim/Diagram/util/ShellCommander.java
+++ CPSim_GUI/src/org/eclipse/cpsim/Diagram/util/ShellCommander.java
@@ -1,5 +1,7 @@
 package org.eclipse.cpsim.Diagram.util;
 
+import java.io.BufferedReader;
+
 public class ShellCommander {
 	public static void shellCmd(String command) throws Exception {
 		Runtime runtime = Runtime.getRuntime();
